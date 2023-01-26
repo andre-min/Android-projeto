a7a841474c5387df00043d142fe03d8ce34e62c5
quartohokage <andre.minato610@gmail.com>
2023-01-26 18:26:00 -0300

    qualquer

--- app/src/androidTest/java/br/com/alura/agenda/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/br/com/alura/agenda/ExampleInstrumentedTest.java
@@ -3,6 +3,8 @@ package br.com.alura.agenda;
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,8 +20,9 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        //Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        //Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("br.com.alura.agenda", appContext.getPackageName());
     }
